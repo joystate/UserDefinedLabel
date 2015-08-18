4d3ecfce037f6fea1a043dd1b0bdf159c647314f
Nadia Yudina <joystate@users.noreply.github.com>
2015-08-18 16:23:38 -0400

    Initial Commit

---
+++ UserDefinedLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserDefinedLabel
+//
+//  Created by Nadia Yudina on 8/18/15.
+//  Copyright (c) 2015 Nadia Yudina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
